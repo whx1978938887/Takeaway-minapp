265dd2f7c8ffed7d127bd21d64434f57b4992ab4
whx1978938887 <whx1978938887@student.gdufe.edu.cn>
2023-07-06 15:26:51 +0800

    >输入框转为步进器 >自定义下拉菜单

--- pages/server/add/add.js
+++ pages/server/add/add.js
@@ -5,6 +5,15 @@ Page({
     fees: 0,
     classify: "",
     food: "",
+    curfId: 4,
+    fruits: [
+      { name: "苹果", id: 1 },
+      { name: "西瓜", id: 2 },
+      { name: "李子", id: 3 },
+      { name: "梨", id: 4 },
+    ],
+    curfruitName: "梨",
+    title: "水果",
   },
   onLoad: function (options) {
     //Do some initialize when page load.
@@ -26,5 +35,9 @@ Page({
   },
   onChangeTap: function (event) {
     console.log(event.detail);
+  },
+  changeFruit: function (e) {
+    console.log(e.detail.selectId);
+    console.log(e.detail.select);
   },
 });
