fac0ebf55c8d2f2d87816866d9b00d9f7a8d5bc8
whx1978938887 <whx1978938887@student.gdufe.edu.cn>
2023-07-12 21:32:40 +0800

    增加订单卡片

--- pages/server/order/order.js
+++ pages/server/order/order.js
@@ -1,4 +1,15 @@
 Page({
+  data: {
+    scrollHeight: 500,
+    date: "",
+    time: "",
+    imageURL: ''
+  },
+  onReady() {
+    let windowHeight = wx.getSystemInfoSync().windowHeight;
+    let scrollHeight = windowHeight - 94;
+    this.setData({ scrollHeight: scrollHeight });
+  },
   onShow() {
     this.getTabBar().init();
   },
